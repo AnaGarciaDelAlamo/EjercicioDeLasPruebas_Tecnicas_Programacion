681a37a3783d7c8866b968c91a81c9e2001b0e96
ana <andelamo@myuax.com>
2023-03-21 22:37:50 +0100

    Metodo devuelveIniciales

--- src/main/java/ClaseCadena.java
+++ src/main/java/ClaseCadena.java
@@ -1,2 +1,19 @@
 public class ClaseCadena {
-}
+    public static String DevuelveIniciales(String nombreCompleto){
+        String iniciales = "";
+        if(nombreCompleto == null || nombreCompleto.isEmpty()){
+            return iniciales;
+        }
+        String[] nombres = nombreCompleto.trim().split("\\s+");
+        for(String nombre : nombres){
+            if(!nombre.isEmpty()){
+                iniciales += nombre.charAt(0) + ".";
+            }
+        }
+        return iniciales.toUpperCase();
+    }
+
+    public static void main(String[] args) {
+
+    }
+        }
